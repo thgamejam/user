ce8adb9a0cd21644804bcec9134bf8b4f330d385
strluck <s@strluck.com>
2022-07-12 15:47:21 +0800

    feat: 修改用户数据模型

--- internal/data/model.go
+++ internal/data/model.go
@@ -4,18 +4,43 @@ import (
 	"github.com/thgamejam/pkg/database"
 )
 
+// User 用户模型
 type User struct {
-	Id               uint32 `json:"id" gorm:"column:id"`
-	Name             string `json:"name" gorm:"column:name"`
-	AccountID        uint32 `json:"account_id" gorm:"column:account_id"`
-	AvatarID         string `json:"avatar_id" gorm:"column:avatar_id"`
-	TagString        string `json:"tags" gorm:"column:tag_string"`
-	AllowSyndication bool   `json:"allow_syndication" gorm:"column:allow_syndication"`
-	WorksCount       uint32 `json:"works_count" gorm:"column:works_count"`
-	FansCount        uint32 `json:"fans_count" gorm:"column:fans_count"`
+	ID               uint32 `json:"id" gorm:"column:id"`
+	AccountID        uint32 `json:"account_id" gorm:"column:account_id"`               // 账户id索引
+	Name             string `json:"name" gorm:"column:name"`                           // 名称
+	AvatarID         uint32 `json:"avatar_id" gorm:"column:avatar_id"`                 // 头像id
+	DisplayTag1      uint16 `json:"display_tag1" gorm:"column:display_tag1"`           // 展示的标签1
+	DisplayTag2      uint16 `json:"display_tag2" gorm:"column:display_tag2"`           // 展示的标签2
+	DisplayTag3      uint16 `json:"display_tag3" gorm:"column:display_tag3"`           // 展示的标签3
+	AllowSyndication bool   `json:"allow_syndication" gorm:"column:allow_syndication"` // 是否允许联合发布邀请
 	database.Model
 }
 
 func (User) TableName() string {
 	return "user"
+}
+
+// UserTagRelational 用户与标签关系
+type UserTagRelational struct {
+	ID     uint32 `json:"id" gorm:"column:id"`
+	UserID uint32 `json:"user_id" gorm:"column:user_id"`         // 用户id
+	TagID  uint16 `json:"user_tag_id" gorm:"column:user_tag_id"` // 用户标签索引
+	Status uint16 `json:"status" gorm:"column:status"`           // 标签状态
+	database.Model
+}
+
+func (UserTagRelational) TableName() string {
+	return "user_tag_relational"
+}
+
+// UserTag 用户标签模型
+type UserTag struct {
+	ID      uint16 `json:"id" gorm:"column:id"`
+	Content string `json:"content" gorm:"column:content"` // 标签内容
+	database.Model
+}
+
+func (UserTag) TableName() string {
+	return "user_tag_enum"
 }
