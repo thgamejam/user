aee2bf8c9dedf751464c0ffdddaccd77033273aa
strluck <s@strluck.com>
2022-07-24 02:11:14 +0800

    feat(data): 增加标签本地缓存类型; 增加用户状态字段; 修改用户名字段

--- internal/data/model.go
+++ internal/data/model.go
@@ -6,16 +6,17 @@ import (
 
 // UserDB 用户模型
 type UserDB struct {
-	ID               uint32 `json:"id" gorm:"column:id"`
+	database.Model
 	AccountID        uint32 `json:"account_id" gorm:"column:account_id"`               // 账户id索引
-	Name             string `json:"name" gorm:"column:name"`                           // 名称
+	Username         string `json:"uname" gorm:"column:uname"`                         // 名称
 	AvatarID         uint32 `json:"avatar_id" gorm:"column:avatar_id"`                 // 头像id
 	Bio              string `json:"bio" gorm:"column:bio"`                             // 个人简介
 	DisplayTag1      uint16 `json:"display_tag1" gorm:"column:display_tag1"`           // 展示的标签1
 	DisplayTag2      uint16 `json:"display_tag2" gorm:"column:display_tag2"`           // 展示的标签2
 	DisplayTag3      uint16 `json:"display_tag3" gorm:"column:display_tag3"`           // 展示的标签3
 	AllowSyndication bool   `json:"allow_syndication" gorm:"column:allow_syndication"` // 是否允许联合发布邀请
-	database.Model
+	// 用户状态, 二进制开关 UserStatus
+	Status uint8 `json:"status" gorm:"column:status"`
 }
 
 func (UserDB) TableName() string {
@@ -24,25 +25,23 @@ func (UserDB) TableName() string {
 
 // UserTagRelationalDB 用户与标签关系
 type UserTagRelationalDB struct {
-	ID     uint32 `json:"id" gorm:"column:id"`
+	database.Model
 	UserID uint32 `json:"user_id" gorm:"column:user_id"`         // 用户id
 	TagID  uint16 `json:"user_tag_id" gorm:"column:user_tag_id"` // 用户标签索引
 	Status uint16 `json:"status" gorm:"column:status"`           // 标签状态
-	database.Model
 }
 
 func (UserTagRelationalDB) TableName() string {
 	return "user_tag_relational"
 }
 
-// UserTagDB 用户标签模型
-type UserTagDB struct {
-	ID      uint16 `json:"id" gorm:"column:id"`
-	Content string `json:"content" gorm:"column:content"` // 标签内容
+// UserTagEnumDB 用户标签模型
+type UserTagEnumDB struct {
 	database.Model
+	Content string `json:"content" gorm:"column:content"` // 标签内容
 }
 
-func (UserTagDB) TableName() string {
+func (UserTagEnumDB) TableName() string {
 	return "user_tag_enum"
 }
 
@@ -50,9 +49,16 @@ func (UserTagDB) TableName() string {
 type UserCache struct {
 	ID        uint32   `json:"id"`         // 用户id
 	AccountID uint32   `json:"account_id"` // 账户id
-	Username  string   `json:"name"`       // 用户名
+	Username  string   `json:"uname"`      // 用户名
 	Bio       string   `json:"bio"`        // 简介
 	Tags      []string `json:"tags"`       // 标签
-	AvatarUrl string   `json:"avatar_url"` // 头像链接
-	AvatarExp int64    `json:"avatar_exp"` // 头像到期时间
+	AvatarUrl string   `json:"image"`      // 头像
+}
+
+// TagLocalCache 标签本地缓存模型
+type TagLocalCache struct {
+	TagID    uint16 // 标签id
+	Content  string // 标签内容
+	IsExist  bool   // 是否存在
+	QueryEXP int64  // 查询失效时间，时间戳
 }
