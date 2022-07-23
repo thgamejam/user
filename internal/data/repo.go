b2384ade583bcc3a98996424e3d436f61d534385
strluck <s@strluck.com>
2022-07-24 03:16:47 +0800

    refactor(data): 独立repo文件处理创建UserRepo

---
+++ internal/data/repo.go
@@ -0,0 +1,42 @@
+package data
+
+import (
+	"context"
+
+	"github.com/go-kratos/kratos/v2/log"
+
+	"user/internal/biz"
+	"user/internal/conf"
+)
+
+type userRepo struct {
+	data     *Data
+	conf     *conf.User
+	tagCache []*TagLocalCache // 用户标签缓存
+
+	log *log.Helper
+}
+
+// NewUserRepo .
+func NewUserRepo(conf *conf.User, data *Data, logger log.Logger) (biz.UserRepo, error) {
+	r := &userRepo{
+		data: data,
+		conf: conf,
+		log:  log.NewHelper(logger),
+	}
+	ctx := context.Background()
+
+	// 读取所有的用户标签，缓存到本地
+	err := r.localCacheSyncTags(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// 生成默认用户头像url
+	err = r.renewDefaultUserAvatarURL(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return r, nil
+}
