b39907617c95dc271eb8c4937c600e1af8f0cf7a
strluck <s@strluck.com>
2022-09-06 00:05:01 +0800

    feat(dev): 增加测试环境的podman和podman-compose支持

--- dev/dev-environment.sh
+++ dev/dev-environment.sh
@@ -3,18 +3,27 @@
 yaml="dev-compose.yaml"
 
 # 判断Docker的环境
+
+# wls2 环境
 docker="docker.exe"
+docker_compose="docker-compose.exe"
 docker_path=$(command -v docker.exe)
+docker_compose_path=$(command -v docker-compose.exe)
+
+# 正常 docker 环境
 if [ -z "$docker_path" ]; then
   docker_path=$(command -v docker)
   docker="docker"
-fi
-
-docker_compose="docker-compose.exe"
-docker_compose_path=$(command -v docker-compose.exe)
-if [ -z "$docker_compose_path" ]; then
   docker_compose_path=$(command -v docker-compose)
   docker_compose="docker-compose"
+fi
+
+# podman 环境
+if [ -z "$docker_path" ]; then
+  docker_path=$(command -v podman)
+  docker="podman"
+  docker_compose_path=$(command -v podman-compose)
+  docker_compose="podman-compose"
 fi
 
 echo "docker path: $docker_path"
