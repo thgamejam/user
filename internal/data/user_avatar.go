bacfdacd354c2ec53f5217ae80e7a435950e8814
strluck <s@strluck.com>
2022-09-05 23:48:46 +0800

    feat(data): 新增 GetUploadAvatarURL获取头像上传链接 方法

--- internal/data/user_avatar.go
+++ internal/data/user_avatar.go
@@ -2,12 +2,35 @@ package data
 
 import (
 	"context"
+	"time"
+
+	"github.com/thgamejam/pkg/authentication"
 	"github.com/thgamejam/pkg/util/strconv"
+	"github.com/thgamejam/pkg/uuid"
 )
 
 const fileSuffix = ".webp"
 
 var _defaultUserAvatarURL string
+
+// GetUploadAvatarURL 获取头像上传链接
+func (r *userRepo) GetUploadAvatarURL(ctx context.Context, userID uint32, crc32 string, sha1 string) (url string, err error) {
+	var claims authentication.UploadFileClaims
+	claims = authentication.UploadFileClaims{
+		Bucket:    r.conf.UserAvatarBucketName,
+		Name:      strconv.UItoa[uint32](userID) + fileSuffix,
+		UUID:      uuid.New(),
+		ExpiresAt: uint32(time.Now().Unix()) + 3600, // TODO 头像到期时间要大于用户信息缓存到期时间。然而用户信息缓存到期时间没写啊
+		CRC:       crc32,
+		SHA1:      sha1,
+	}
+
+	uploadURL, err := authentication.CreateUploadURL(&claims, &r.conf.UploadFileServiceSecretKey)
+	if err != nil {
+		return "", err
+	}
+	return uploadURL, nil
+}
 
 // GetUserAvatarURL 根据用户ID获取头像
 func (r *userRepo) GetUserAvatarURL(ctx context.Context, avatarID uint32) (string, error) {
